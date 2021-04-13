7184367a730a22e4afb6fb425d3c8d5f5a105907
Admin <Admin@Admin-PC>
2021-04-13 21:06:31 +0530

    [Dipali] push code to navtej

--- src/deca/sample23.java
+++ src/deca/sample23.java
@@ -4,6 +4,7 @@ public class sample23 {
 
 	public static void main(String[] args) {
 		System.out.println("Good morning");
-		system.out.println("good afternoon");
+		System.out.println("good afternoon");
+		System.out.println("hiiiii  good night ");
 	}
 }
