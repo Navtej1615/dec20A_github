6ccd5288ed8f6e300cc8c803718a2eb7978fb64a
Ashish <Ashish@LAPTOP-TOD4QBS9>
2021-05-16 22:03:26 +0530

    navtej code 16 may 2021

--- src/deca/sample23.java
+++ src/deca/sample23.java
@@ -6,5 +6,8 @@ public class sample23 {
 		System.out.println("Good morning");
 		System.out.println("good afternoon");
 		System.out.println("hiiiii  good night ");
+		
+		
+		System.out.println("nnnnnnnnnnnnewwwwwwwwwwww 16may2021");
 	}
 }
